676365aee65ea424c7131875214e0e855675bdb8
DucTheVulpe <41674548+DucTheVulpe@users.noreply.github.com>
2024-04-25 23:36:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 API_HASH = os.getenv("API_HASH")
 API_ID = int(os.getenv("API_ID"))
 PREFIX = os.getenv("PREFIX")
+MONGO_URI = os.getenv("MONGO_URI")
+LOG_CHAT = int(os.getenv("LOG_CHAT"))
